b496ef2b1be4be7423984e49f38a7dbada1f213f
Viktor Khon <betepokus@yahoo.com>
2017-08-03 15:57:45 -0400

    Create project and Book.java

---
+++ app/src/main/java/viktorkhon/com/udacity_project_7_booklistingapp/Book.java
@@ -0,0 +1,46 @@
+package viktorkhon.com.udacity_project_7_booklistingapp;
+
+/**
+ * Created by Viktor Khon on 8/3/2017.
+ */
+
+public class Book {
+
+    private String mTitle;
+
+    private String mAuthors;
+
+    private String mPrice;
+
+    private String mPublisher;
+
+    private String mUrl;
+
+    public Book(String title, String authors, String price, String publisher, String url) {
+        mTitle = title;
+        mAuthors = authors;
+        mPrice = price;
+        mPublisher = publisher;
+        mUrl = url;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getAuthors() {
+        return mAuthors;
+    }
+
+    public String getPrice() {
+        return mPrice;
+    }
+
+    public String getPublisher() {
+        return mPublisher;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+}
